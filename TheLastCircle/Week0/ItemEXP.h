d3cd49fc25fe4a179d3465a738aecc5404da0952
minggong222 <minggong222@naver.com>
2026-09-01 18:24:08 +0900

    EXP 변수 추가

--- TheLastCircle/Week0/ItemEXP.h
+++ TheLastCircle/Week0/ItemEXP.h
@@ -5,9 +5,17 @@ class UItemEXP : public UItem
 {
 public:
 	float Reword;
+	bool bIsFollow = false;
+	bool bIsActive = true;
 	UItemEXP(float reword) : UItem()
 	{
 		Reword = reword;
-
+		ItemType = ETI_EXP;
+	}
+	void Die() {
+		bIsActive = false;
+	}
+	bool IsActive() {
+		return bIsActive;
 	}
 };
