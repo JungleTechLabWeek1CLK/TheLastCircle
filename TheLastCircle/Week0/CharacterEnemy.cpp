59a6423feaf8f407213a26527d546bbe61f5642f
이재용(JaeyongLEE) <roqkfwk7613@gmail.com>
2026-09-03 12:00:46 +0900

    Update CharacterEnemy.cpp

--- TheLastCircle/Week0/CharacterEnemy.cpp
+++ TheLastCircle/Week0/CharacterEnemy.cpp
@@ -22,8 +22,19 @@ void UCharacterEnemy::Move(FVector location, float delta) {
 
     float d = sqrt((Location.x - location.x) * (Location.x - location.x) + (Location.y - location.y) * (Location.y - location.y));
 
+    if (KnockbackTimer > 0.f)
+    {
+        Location.x += KnockbackDirection.x * 5.1f * delta;
+        Location.y += KnockbackDirection.y * 5.1f * delta;
+        KnockbackTimer -= delta;
+    }
+    else 
+    {
+        UCharacter::Move(location, delta);
+        float d = sqrt((Location.x - location.x) * (Location.x - location.x) + (Location.y - location.y) * (Location.y - location.y));
         if (d <= Range && Delay <= ShootTime)
             Attack(location);
+    }
 }
 
 void UCharacterEnemy::Attack(FVector location) {
